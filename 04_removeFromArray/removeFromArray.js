24a9a83e49815a4d821413153fc5b25fb10ea8d0
anyalauria <anyalauria@gmail.com>
2022-06-10 21:14:49 -0700

    remove from array solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
-
+    let args = Array.from(arguments);
+    let arr = args[0]
+    args.shift();
+    for(let i = 0; i < args.length; i++){
+        for( let j = 0; j < arr.length; j++){
+            if (args.includes(arr[j])){
+                arr.splice(j,1);
+                j--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
